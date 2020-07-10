5e9c76327d7c072f130f6ead966667f6a8a4691e
Anais Assamoi <anaisassamoi02@gmail.com>
2020-07-10 18:16:26 +0200

    minors changes

--- js/Burgermenu.js
+++ js/Burgermenu.js
@@ -1,26 +1,24 @@
-var BurgerMenu = 
-{
+var BurgerMenu = {
+  burger: document.getElementById("bars"),
+  menu: document.getElementById("menu"),
+  estFerme: true,
 
-	burger : document.getElementById("bars"),
-	menu : document.getElementById("menu"),
-	estFerme : true,
-
-	initBurger : function() {
-		this.burger.addEventListener("click", function() {
-			if (BurgerMenu.estFerme == true) {
-				BurgerMenu.menu.style.display = "block";
-				BurgerMenu.estFerme = false;				
-			}else if (BurgerMenu.estFerme == false) {
-				BurgerMenu.menu.style.display = "none";
-				BurgerMenu.estFerme = true;				
-			}
-		    
-		});
-		this.menu.addEventListener("click", function() {
-			BurgerMenu.menu.style.display = "none";
-			BurgerMenu.estFerme = true;
-		});
-		
-	}
+  initBurger: function () {
+    this.burger.addEventListener("click", function () {
+      if (BurgerMenu.estFerme == true) {
+        BurgerMenu.menu.style.display = "block";
+        BurgerMenu.estFerme = false;
+      } else if (BurgerMenu.estFerme == false) {
+        BurgerMenu.menu.style.display = "none";
+        BurgerMenu.estFerme = true;
+      }
+    });
+    this.menu.addEventListener("click", function () {
+      BurgerMenu.menu.style.display = "none";
+      BurgerMenu.estFerme = true;
+    });
+  },
 };
-BurgerMenu.initBurger();
+if (window.matchMedia("(max-width: 730px)").matches) {
+  BurgerMenu.initBurger();
+}
